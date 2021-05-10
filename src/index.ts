2b1eec43b9d570a40981defbaca604ee50583a25
yi-fan-song <yfsong00@gmail.com>
2021-05-09 20:57:24 -0400

    added nakirium button

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("hi");
+const nakiriBtn = document.getElementById("nakirium-button");
+if (nakiriBtn !== undefined && nakiriBtn !== null) {
+	nakiriBtn.onclick = getNakirium;
+}
+
+function getNakirium(e: MouseEvent) {
+	const nakiriPlayer = document.createElement("iframe");
+
+	nakiriPlayer.setAttribute("title", "Nakirium");
+	nakiriPlayer.setAttribute("frameborder", "0");
+	nakiriPlayer.setAttribute(
+		"allow",
+		"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	);
+	nakiriPlayer.setAttribute("allowfullscreen", "");
+
+	nakiriPlayer.classList.add("grid-center");
+	nakiriPlayer.id = "nakirium-player";
+
+	const youtubeSrc = "https://www.youtube.com/embed/NPMhb_mIIhU";
+
+	nakiriPlayer.setAttribute("src", youtubeSrc);
+
+	(e.target as HTMLElement).parentElement?.parentElement?.appendChild(
+		nakiriPlayer
+	);
+	(e.target as HTMLElement).parentElement?.remove();
+}
