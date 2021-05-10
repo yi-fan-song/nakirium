6f45139a17f87ab54b527ff33fb790a58f1abc5b
yi-fan-song <44707828+yi-fan-song@users.noreply.github.com>
2021-05-10 01:38:05 +0000

    Deploying to release from @ yi-fan-song/nakirium@6ef5e39807b6c8e1f45f1e217264f945b32b7d68 🚀

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const nakiriBtn = document.getElementById("nakirium-button");
 if (nakiriBtn !== undefined && nakiriBtn !== null) {
     nakiriBtn.onclick = getNakirium;
 }
+const nakiriumSource = ["NPMhb_mIIhU", "eMIXyxt2xv4", "EVTIYVwYWz4"];
 function getNakirium(e) {
     var _a, _b, _c;
     const nakiriPlayer = document.createElement("iframe");
@@ -12,9 +13,18 @@ function getNakirium(e) {
     nakiriPlayer.setAttribute("allowfullscreen", "");
     nakiriPlayer.classList.add("grid-center");
     nakiriPlayer.id = "nakirium-player";
-    const youtubeSrc = "https://www.youtube.com/embed/NPMhb_mIIhU";
+    let youtubeSrc = "https://www.youtube.com/embed/";
+    const startTime = getRandomInt(570);
+    youtubeSrc += nakiriumSource[getRandomInt(3)];
+    youtubeSrc += "?start=";
+    youtubeSrc += startTime;
+    youtubeSrc += "&end=";
+    youtubeSrc += startTime + 30;
     nakiriPlayer.setAttribute("src", youtubeSrc);
     (_b = (_a = e.target.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.appendChild(nakiriPlayer);
     (_c = e.target.parentElement) === null || _c === void 0 ? void 0 : _c.remove();
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 //# sourceMappingURL=index.js.map
